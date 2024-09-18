05a134723f8794ad917bfad077f6b700c67a338c
Lolliiimor <rmorin@africaexchange.com>
2024-09-18 23:05:48 +0100

    Initial commit from Create Next App

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,tsx,jsx,mdx}",
+    "./src/components/**/*.{js,tsx,jsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
